55df1cf426b73dfdc0a7c0fec17e52e0a11db791
nitesh <dandy13761@gmail.com>
2022-10-11 23:53:00 -0700

    initial sass setup

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -5,7 +5,6 @@ import { Component, OnInit } from '@angular/core';
   // selector: '.app-nav', --can be used as a class
   selector: 'app-nav', //--can be used as an element
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
   constructor() {}
